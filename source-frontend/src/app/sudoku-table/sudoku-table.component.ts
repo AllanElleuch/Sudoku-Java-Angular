7be2b3f7847acc94c21120e716faf83fc8c4a88a
Allan Elleuch <allan.elleuch@gmail.com>
2020-02-22 16:11:48 +0100

    add base project for angular front-end, add sudoku component

---
+++ source-frontend/src/app/sudoku-table/sudoku-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sudoku-table',
+  templateUrl: './sudoku-table.component.html',
+  styleUrls: ['./sudoku-table.component.css']
+})
+export class SudokuTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
