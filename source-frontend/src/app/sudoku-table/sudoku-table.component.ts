4fa9483d5972e70a264ebacc70d7a1a747db96ac
Allan Elleuch <allan.elleuch@gmail.com>
2020-02-22 17:11:14 +0100

    add js to handle input

--- source-frontend/src/app/sudoku-table/sudoku-table.component.ts
+++ source-frontend/src/app/sudoku-table/sudoku-table.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as $ from "jquery";
 
 @Component({
-  selector: 'app-sudoku-table',
-  templateUrl: './sudoku-table.component.html',
-  styleUrls: ['./sudoku-table.component.css']
+  selector: "app-sudoku-table",
+  templateUrl: "./sudoku-table.component.html",
+  styleUrls: ["./sudoku-table.component.css"]
 })
 export class SudokuTableComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    $(document).ready(function() {
+      // Add event listenner on cell input, cell input will be send to the java backend for confirmation of input
+      $("td").keyup(function(event) {
+        let cell = event.currentTarget;
+        let cellIndex = cell.getAttribute("cell-index");
+        cellIndex = parseInt(cellIndex, 10);
+        let rowIndex = Math.floor(cellIndex / 9);
+        let cellIndexInRow = cellIndex - rowIndex * 9;
+        console.log(`current input on : 
+        - cellIndex : ${cellIndex}
+        - rowIndex : ${rowIndex}
+        - cellIndexInRow : ${cellIndexInRow}
+        - currentValue : ${cell.innerText}`);
 
+        // todo send ajax call to back end
+        let resultForInputFromBackend = false;
+
+        if (!resultForInputFromBackend) {
+          $(cell).addClass("incorrect-input");
+        } else {
+          $(cell).removeClass("incorrect-input");
+        }
+      }); // td keyup
+    });
+  }
 }
