8830b22f017a769bfa68609b5c99d06cc7e41fe1
Allan Elleuch <allan.elleuch@soprasteria.com>
2020-02-25 13:58:31 +0100

    delete unused import

--- source-backend/src/main/java/com/example/demo/sudokuController.java
+++ source-backend/src/main/java/com/example/demo/sudokuController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.howtodoinjava.demo.model.Employee;
-
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
